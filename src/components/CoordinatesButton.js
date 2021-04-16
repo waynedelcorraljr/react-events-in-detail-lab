724fddef107fdbeaf4868381bcfd37e3918d971b
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-04-16 02:35:00 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,13 @@
+
+
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+export default class CoordinatesButton extends Component {
+
+    render() {
+        return(
+            <button onClick={(event) => this.props.onReceiveCoordinates([event.clientX, event.clientY])}>coordinates button</button>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,16 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+export default class DelayedButton extends Component {
+
+    
+
+    render() {
+        return(
+            <button 
+            onClick={(event) => setTimeout(this.props.onDelayedClick, this.props.delay, event)}>
+                Delayed Button
+            </button>
+        )
+    }
+}
